8024d30afa60f0ce09e1e730d742c394482419ac
Lee <20373159@buaa.edu.cn>
2022-03-17 14:30:35 +0800

    1

---
+++ find_warnings.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+gcc -Wall $1 -o test 2>warning.txt
+x=$?
+grep  "warning" warning.txt | sed -n 's/warning: //g' >> result.txt
+if [ $x -eq 0 ] 
+then
+i=1
+while [ $i -le $2 ]
+do
+test $i >> result.txt 
+i=$[$i+1] 
+done
+fi
+pwd >> result.txt
