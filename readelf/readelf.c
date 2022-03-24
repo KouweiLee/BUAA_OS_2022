c05265df3e693c8c91a69009e38c7122e938da4c
Lee <20373159@buaa.edu.cn>
2022-03-24 14:18:49 +0800

    2

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -64,11 +64,11 @@ int readelf(u_char *binary, int size)
 
 	// get section table addr, section header number and section header size.
 	ptr_sh_table = (ehdr -> e_shoff) + binary;
-	/*sh_entry_count = ehdr -> e_shnum;
+	sh_entry_count = ehdr -> e_shnum;
 	sh_entry_size = ehdr -> e_shentsize;
 	// for each section header, output section number and section addr. 
 	// hint: section number starts at 0.
-	for(Nr=0;Nr<sh_entry_count;Nr++){
+	/*for(Nr=0;Nr<sh_entry_count;Nr++){
 		shdr = (Elf32_Shdr *) (ptr_sh_table + Nr * sh_entry_size);//section table address 
 		printf("%d:0x%x\n",Nr,shdr -> sh_addr);
 	}*/
