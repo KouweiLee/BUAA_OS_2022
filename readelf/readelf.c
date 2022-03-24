1ba1f5f2977a4f647da19128a7b411868deaa737
Lee <20373159@buaa.edu.cn>
2022-03-24 15:30:06 +0800

    a1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -75,16 +75,18 @@ int readelf(u_char *binary, int size)
 	
 	for(Nr=0;Nr<count-1;Nr++){
 		phdr1 = (Elf32_Phdr *)(ptr_ph_table + Nr*psize);
-		phdr2 = phdr1 + 1;
+		for(int i = Nr+1;i<count;i++){
+			phdr2 = (Elf32_Phdr *)(ptr_ph_table + i*psize);
 		Elf32_Addr l2 = phdr2->p_vaddr;
 		Elf32_Addr r1 = phdr1->p_vaddr + phdr1->p_memsz - 1;
-		if(r1 >= l2){
+		if(r1 > l2){
 			printf("Conflict at page va : 0x%x\n", (l2 & 0xfffff000));
 			return 0;
 		}
 		if((r1 & 0xfffff000) == (l2 & 0xfffff000)){
 			printf("Overlay at page va : 0x%x\n", (l2 & 0xfffff000));
 			return 0;
+		}
 		}
 	}
 	for(Nr=0;Nr<count;Nr++){
