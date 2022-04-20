db61677d6d90a980c8dbccc49ef20b15d112f73c
Lee <20373159@buaa.edu.cn>
2022-04-20 15:58:35 +0800

    4/20/1

--- mm/pmap.c
+++ mm/pmap.c
@@ -123,7 +123,7 @@ void boot_map_segment(Pde *pgdir, u_long va, u_long size, u_long pa, int perm)
             1 /* create if entry of page directory not exists yet */
         );
         /* Step 2. fill in the page table */
-        *pgtable_entry = ((pa+i))
+        *pgtable_entry = (PTE_ADDR(pa+i))
                         | perm | PTE_V;
     }
 }
