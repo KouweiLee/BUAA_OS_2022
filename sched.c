88f20478bb5352abfbe422d80f80e6fb41f8beea
Lee <20373159@buaa.edu.cn>
2022-05-11 08:44:06 +0800

    839

--- sched.c
+++ sched.c
@@ -20,40 +20,39 @@ void SJF ( int number_of_jobs, const int job_submitted_time [],
            const int job_required_time [], int job_sched_start [] ){
 	int i, time;
 	struct Mist *head = NULL;
-	struct Mist *tmp;
-	struct Mist *p;
+	struct Mist *tmp = NULL;
+	struct Mist *p = NULL;
 	head = (struct Mist*)malloc(sizeof(struct Mist));
 	head->id = 0;
 	head->time = job_required_time[0];
 	head->next = NULL;
 	for(i=1;i<number_of_jobs;i++){
 		time = job_required_time[i];
-		p = (struct Mist*) malloc(sizeof(struct Mist));
+		p = (struct Mist*)malloc(sizeof(struct Mist));
 		p->id = i;
 		p->time = time;
-//		printf("yes\n");
+		p->next = NULL;
 		for(tmp = head; tmp->next != NULL && time >= tmp->next->time; tmp = tmp->next);
-//		printf("if4\n");
 		if(tmp == head){
 			if(time >= head->time){
+	//			head->next = p;
+				p->next = head->next;
 				head->next = p;
-				p->next = NULL;
-//				printf("if1\n");
+	//			printf("1\n");
 				continue;
 			}
 			p->next = head;
 			head = p;
-//			printf("if2\n");
+	//		printf("2\n");
 		} else{
 			p->next = tmp->next;
 			tmp->next = p;
-//			printf("if3\n");
-//			printf("%d\n", i);
+	//		printf("3\n");
 		}
 	}
 	p=head;
 	job_sched_start[p->id] = job_submitted_time[0];
-	for(i=0;i<number_of_jobs - 1 ;i++){
+	for(i=0;i<number_of_jobs-1 ;i++){
 		time = job_sched_start[p->id] + job_required_time[p->id];
 		p=p->next;
 		job_sched_start[p->id] = time < job_submitted_time[p->id] ? job_submitted_time[p->id] : time;
