87be26ff110f3606b579ab7f69c902e5fc1213dd
Lee <20373159@buaa.edu.cn>
2022-05-11 17:04:03 +0800

    5/11

--- sched.c
+++ sched.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 void FCFS ( int number_of_jobs, const int job_submitted_time [],
-			const int job_required_time [], int job_sched_start []){
+		const int job_required_time [], int job_sched_start []){
 	int i, tmp;
 	job_sched_start[0] = job_submitted_time[0];
 	for(i = 0;i < number_of_jobs - 1; i++){
@@ -17,44 +17,56 @@ struct Mist{
 	struct Mist *next;
 };
 void SJF ( int number_of_jobs, const int job_submitted_time [],
-           const int job_required_time [], int job_sched_start [] ){
-	int i, time;
-	struct Mist *head = NULL;
-	struct Mist *tmp = NULL;
-	struct Mist *p = NULL;
-	head = (struct Mist*)malloc(sizeof(struct Mist));
-	head->id = 0;
-	head->time = job_required_time[0];
-	head->next = NULL;
-	for(i=1;i<number_of_jobs;i++){
-		time = job_required_time[i];
-		p = (struct Mist*)malloc(sizeof(struct Mist));
-		p->id = i;
-		p->time = time;
-		p->next = NULL;
-		for(tmp = head; tmp->next != NULL && time >= tmp->next->time; tmp = tmp->next);
-		if(tmp == head){
-			if(time >= head->time){
-	//			head->next = p;
-				p->next = head->next;
-				head->next = p;
-	//			printf("1\n");
-				continue;
-			}
-			p->next = head;
-			head = p;
-	//		printf("2\n");
-		} else{
-			p->next = tmp->next;
-			tmp->next = p;
-	//		printf("3\n");
+		const int job_required_time [], int job_sched_start [] ){
+	job_sched_start[0] = job_submitted_time[0];
+	int id, now = 1, i, time;
+	int finish = job_sched_start[0] + job_required_time[0];
+	struct Mist *head =NULL, *p=NULL, *tmp=NULL;
+	while(now != number_of_jobs){
+		if(head == NULL && finish < job_submitted_time[now]){
+			finish = job_submitted_time[now];
 		}
+		for(i=now;finish>=job_submitted_time[i] && i < number_of_jobs;i++){
+		//	LIST_INSERT(i);
+			time = job_required_time[i];
+			p = (struct Mist*) malloc(sizeof(struct Mist));
+			p->id = i;
+			p->time = time;
+			p->next = NULL;
+			if(head == NULL) head = p;
+		    else {
+				for(tmp=head;tmp->next!=NULL&&tmp->next->time <= time;tmp = tmp->next);
+				if(tmp == head){
+					if(head->time <= time){
+						p->next = head->next;
+						head->next = p;
+					} else {
+						p->next = head;
+						head = p;
+					}
+				}else {
+					p->next = tmp->next;
+					tmp->next = p;
+				}
+			}
+		}
+		if(head != NULL){
+			p = head;
+			id = p->id;
+			head = head->next;
+			free(p);
+			job_sched_start[id] = finish;
+			finish = job_sched_start[id] + job_required_time[id];
+			now = i;
+		}
+	}       	
+	while(head!=NULL){
+		p = head;
+		id = p->id;
+		head = head->next;
+		free(p);
+		job_sched_start[id] = finish;
+		finish = job_sched_start[id] + job_required_time[id];
 	}
-	p=head;
-	job_sched_start[p->id] = job_submitted_time[0];
-	for(i=0;i<number_of_jobs-1 ;i++){
-		time = job_sched_start[p->id] + job_required_time[p->id];
-		p=p->next;
-		job_sched_start[p->id] = time < job_submitted_time[p->id] ? job_submitted_time[p->id] : time;
-	}
+
 }
