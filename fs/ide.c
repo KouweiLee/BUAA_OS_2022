3a003950535680f034e36e93fe38c5c647fb222b
Lee <20373159@buaa.edu.cn>
2022-06-02 15:07:41 +0800

    1

--- fs/ide.c
+++ fs/ide.c
@@ -94,3 +94,77 @@ ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 		// if error occur, then panic.
 	}
 }
+
+int raid4_valid(u_int diskno){
+	u_int now = 0;
+	u_int zero = 0;
+	int r;
+	syscall_write_dev(&diskno, 0x13000010, 4);
+	syscall_write_dev(&now, 0x13000000, 4);
+	syscall_write_dev(&zero, 0x13000020, 4);
+	syscall_read_dev(&r, 0x13000030, 4);
+	if(r == 0){//´æÔÚÒì³£
+	return 0;
+	}
+	return 1;
+}
+
+int raid4_write(u_int blockno, void *src){
+	char *begin = (char*) src;
+	u_int secno = 2*blockno;
+	int i, r = 0;
+	//char a0[512] = begin;
+	//char a1[512] = being + 0x200;
+	char p0[512], p1[512];
+	for(i=0;i<512;i++){
+		p0[i] = begin[i] ^ begin[i+0x200] ^ begin[i+0x200*2] ^ begin[i+0x200*3];
+		p1[i] = begin[i+0x200*4] ^ begin[i+0x200*5] ^ begin[i+0x200*6] ^ begin[i+0x200*7];
+	}
+	for(i=1;i<=4;i++){
+		if(raid4_valid(i)){
+			ide_write(i, secno, begin+0x200*(i-1), 1);
+			ide_write(i, secno+1, begin+0x200*(i+3), 1);
+		}else {
+			r++;
+		}
+	}
+	if(raid4_valid(5)){
+		ide_write(5, secno, p0, 1);
+		ide_write(5, secno+1, p1, 1);
+	}else {
+		r++;
+	}
+	return r;
+}
+
+int raid4_read(u_int blockno, void *dst){
+	char *begin = (char*)dst;
+	u_int secno = 2*blockno;
+	int i, r=0;
+	char p0[512], p1[512];
+	for(i=1;i<=5;i++){
+		if(!raid4_valid(i)){
+			r++;
+		}
+	}
+	if(r>0){
+		return r;
+	}
+	//ÎÞ´ÅÅÌËð»µ
+	for(i=1;i<=4;i++){
+		ide_read(i, secno, begin+0x200*(i-1), 1);
+		ide_read(i, secno+1, begin + 0x200*(i+3), 1);
+	}
+	user_bzero((void *)p0, 512);
+	user_bzero((void *)p1, 512);
+	ide_read(5, secno, p0, 1);
+	ide_read(5, secno+1, p1, 1);
+	for(i=0;i<512;i++){
+		char aa = begin[i] ^ begin[i+0x200] ^ begin[i+0x200*2] ^ begin[i+0x200*3];
+		char ab = begin[i+0x200*4] ^ begin[i+0x200*5] ^ begin[i+0x200*6] ^ begin[i+0x200*7];
+		if(!(p0[i] == aa && p1[i] == ab)){
+			return -1;
+		}
+	}
+	return 0;
+}
